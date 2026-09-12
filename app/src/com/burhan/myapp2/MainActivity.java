526483e86f3488e08182369b8537c1a0238065c9
BurhanMughal01 <buisch2314@gmail.com>
2026-09-13 03:30:24 +0500

    Update MainActivity.java

--- app/src/com/burhan/myapp2/MainActivity.java
+++ app/src/com/burhan/myapp2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.burhan.myapp2;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
@@ -10,13 +12,29 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Window کا پس منظر سیاہ کریں (white flash سے بچنے کے لیے)
+        getWindow().setBackgroundDrawable(
+            new ColorDrawable(Color.parseColor("#0a0a1a"))
+        );
+        
         WebView webView = new WebView(this);
+        
+        // WebView کا background بھی سیاہ کریں
+        webView.setBackgroundColor(Color.parseColor("#0a0a1a"));
+        
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
         settings.setAllowFileAccess(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
+        settings.setAllowContentAccess(true);
+        
+        // WebView transparent سیٹنگ نہ کریں
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("file:///android_asset/index.html");
+        
         setContentView(webView);
     }
 }
