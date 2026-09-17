9540c6e0c55265d14c3ec05bb584547500da5a53
Burhan Ahmad Mughal <burhan@bamstudios.com>
2026-09-17 10:23:30 +0500

    Professional Adhkar - morning/evening with counters, progress tracking

---
+++ inject_adhkar.py
@@ -0,0 +1,23 @@
+with open('adhkar_data.txt') as f:
+    data = f.read()
+with open('adhkar_js.txt') as f:
+    js = f.read()
+with open('templates/adhkar.html') as f:
+    content = f.read()
+
+old = '''<script>
+/* PART 2 WILL BE INJECTED */
+</script>'''
+
+new = '<script>\n' + data + '\n\n' + js + '\n</script>'
+
+if old in content:
+    content = content.replace(old, new)
+    print('Injected OK')
+else:
+    print('Placeholder not found!')
+
+with open('templates/adhkar.html', 'w', encoding='utf-8') as f:
+    f.write(content)
+
+print('Total size:', len(content), 'bytes')
